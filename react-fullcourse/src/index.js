cdafd90bad0120f9a36835c674d931dd51a14d7c
gnganapath <gnganapathi@gmail.com>
2020-08-09 20:07:04 +0530

    begin - create react app- video no12 end

--- react-fullcourse/src/index.js
+++ react-fullcourse/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 class Hello extends React.Component {
@@ -12,8 +12,8 @@ class Hello extends React.Component {
   }
 }
 
-// ReactDOM.render(  <React.StrictMode>    <App />  </React.StrictMode>,  document.getElementById('root'));
-ReactDOM.render(     <Hello />  ,  document.getElementById('root'))
+ReactDOM.render(  <React.StrictMode>    <App />  </React.StrictMode>,  document.getElementById('root'));
+//ReactDOM.render(     <Hello />  ,  document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

040127c5c5fdf5cf139166b823c1146f365f41fa
gnganapath <gnganapathi@gmail.com>
2020-10-13 13:33:25 +0530

    React JS Full course Learning path - course topic video categories

--- react-fullcourse/src/App.js
+++ react-fullcourse/src/App.js
@@ -4,22 +4,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h2>React JS Full course - Course particians- Video</h2>
+    <ul>
+      <li>No.13-35 = class based component - react</li>
+      <li>No.36-41 = Router</li>
+      <li>42-44 = Weather App</li>
+      <li>45 = youtube app</li>
+      <li>46-49 = Blog App</li>
+      <li>No.50-79 = Hooks & context</li>
+      <li>No.80 - 89 = FSD - shopping Kart app</li>
+      <li>90- 95 = Flicker App</li>
+      <li>No.96-99 = Neflix App</li>
+      <li>No.104-128 = Formik</li>
+      <li>No.129 - 151 = Redux, contect, Thunk</li>
+    </ul>
+    </>
   );
 }
 

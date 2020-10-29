d63794d1fb6396fb23ab5467c1625cdb09ba19a7
gnganapath <gnganapathi@gmail.com>
2020-10-29 23:24:14 +0530

    material UI added, Router with Home component, redirect to home

--- react-fullcourse/src/App.js
+++ react-fullcourse/src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch, Redirect,BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Child from './components/child';
+import Home from './components/home';
+import About from './components/about';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function App(props) {
+  const classes = useStyles();
   console.log(props.data);
   return (
     <>
     <h2>React JS Full course - Course particians- Video</h2>
-    <ul>
-      <li>No.13-35 = class based component - react</li>
-      <li>No.36-41 = Router</li>
-      <li>42-44 = Weather App</li>
-      <li>45 = youtube app</li>
-      <li>46-49 = Blog App</li>
-      <li>No.50-79 = Hooks & context</li>
-      <li>No.80 - 89 = FSD - shopping Kart app</li>
-      <li>90- 95 = Flicker App</li>
-      <li>No.96-99 = Neflix App</li>
-      <li>No.104-128 = Formik</li>
-      <li>No.129 - 151 = Redux, contect, Thunk</li>
-    </ul>
+    <BrowserRouter>
+      <Switch>
+         
+
+          <div className={classes.root}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Paper className={classes.paper}>xs=12</Paper>
+                <Route exact path="/">    <Redirect to="/Home" />  </Route>
+
+                  <Route exact path="/Home" component={Home} />
+
+                  <Route exact path="/About" component={About} />
+              </Grid>
+              <Grid item xs={6}>
+                <Paper className={classes.paper}>xs=6</Paper>
+              </Grid>
+              <Grid item xs={6}>
+                <Paper className={classes.paper}>xs=6</Paper>
+              </Grid>
+              <Grid item xs={3}>
+                <Paper className={classes.paper}>xs=3</Paper>
+              </Grid>
+              <Grid item xs={3}>
+                <Paper className={classes.paper}>xs=3</Paper>
+              </Grid>
+              <Grid item xs={3}>
+                <Paper className={classes.paper}>xs=3</Paper>
+              </Grid>
+              <Grid item xs={3}>
+                <Paper className={classes.paper}>xs=3</Paper>
+              </Grid>
+            </Grid>
+          </div>
+      </Switch>
+    </BrowserRouter>
     <Child data={props.data}/>
     </>
   );
---
+++ react-fullcourse/src/components/home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            Home Component
+            <ul>
+                <li>No.13-35 = class based component - react</li>
+                <li>No.36-41 = Router</li>
+                <li>42-44 = Weather App</li>
+                <li>45 = youtube app</li>
+                <li>46-49 = Blog App</li>
+                <li>No.50-79 = Hooks & context</li>
+                <li>No.80 - 89 = FSD - shopping Kart app</li>
+                <li>90- 95 = Flicker App</li>
+                <li>No.96-99 = Neflix App</li>
+                <li>No.104-128 = Formik</li>
+                <li>No.129 - 151 = Redux, contect, Thunk</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Home
